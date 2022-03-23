1011ea066b9cbf262ad51e872ad13e95bdaed15c
Michal Wozniak <michalw875@gmail.com>
2022-03-23 21:05:28 +0100

    inicjalizacja projektu

---
+++ MusicQuiz/src/main/java/com/example/MusicQuiz/MusicQuizApplication.java
@@ -0,0 +1,13 @@
+package com.example.MusicQuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicQuizApplication.class, args);
+	}
+
+}
---
+++ MusicQuiz/src/test/java/com/example/MusicQuiz/MusicQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MusicQuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
