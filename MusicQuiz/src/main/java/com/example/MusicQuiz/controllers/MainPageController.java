fbe10730355a27a48d7deb6938b06cea2f87c04b
Michal Wozniak <michalw875@gmail.com>
2022-03-27 19:50:17 +0200

    dodanie strony pocztkowej

--- MusicQuiz/src/main/java/com/example/MusicQuiz/controllers/MainPageController.java
+++ MusicQuiz/src/main/java/com/example/MusicQuiz/controllers/MainPageController.java
@@ -1,4 +1,14 @@
 package com.example.MusicQuiz.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
 public class MainPageController {
+    @GetMapping("/")
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView("start");
+        return mv;
+    }
 }
---
+++ MusicQuiz/src/main/java/com/example/MusicQuiz/controllers/QuestionController.java
@@ -0,0 +1,4 @@
+package com.example.MusicQuiz.controllers;
+
+public class QuestionController {
+}
